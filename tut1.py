2a8081b5bca86a4480e14544668444bb47462fff
Chinmay Kulthe <chinmaykulthe897@gmail.com>
2021-06-02 21:29:38 +0530

    tut2 added

--- tut1.py
+++ tut1.py
@@ -1,3 +1,11 @@
+# This a single line comment
+"""
+This 
+a 
+multi 
+line 
+comment
+"""
 print("Hello world")
 print("Chinmay\tKulthe")
 print("Python\nC\nC++\nC#\nJS")
---
+++ tut2.py
@@ -0,0 +1,38 @@
+# Variables
+myAge = 14
+iAm = "Chinmay Kulthe"
+print("I am " + iAm + " years " + str(myAge) + " old")
+
+# Type conversion
+x = str(14)
+y = int(14.963)
+z = float(34)
+print(x)
+print(y)
+print(z)
+
+# Geting type of a variable
+print(type(myAge))
+print(type(iAm))
+
+# Multiple values to multiple variables
+lang1, lang2, lang3 = "C++","C","Python"
+print(lang1)
+print(lang2)
+print(lang3)
+
+# One value to multiple variables
+a = b = c = "JavaScript"
+print(a)
+print(b)
+print(c)
+print(c)
+
+# Variable Name Rules -
+"""
+- A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+- A variable name must start with a letter or the underscore character
+- A variable name cannot start with a number
+- A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+- Variable names are case-sensitive (age, Age and AGE are three different variables)
+"""
