00651be8d355a47fb402cd70e5230c70a6dd6ec8
Chinmay Kulthe <chinmaykulthe897@gmail.com>
2021-06-03 19:33:02 +0530

    add more code to tut4.py

--- tut4.py
+++ tut4.py
@@ -1,6 +1,7 @@
 l = ["C","C++","Python","Java","C#"]
 print(l)
 print(l[0])
+print(l.count("C++"))
 n = [24, 64, 19, 34]
 # n.sort()
 # n.reverse()
@@ -29,3 +30,12 @@ n1, n2 = n2, n1
 # n1 = n2
 # n2 = temp
 print(n1, n2)
+
+l2 = ["Minecraft","Krunker","Just Cause 2","Farcry3"]
+l3 = ["Best","Good","No idea","No idea"]
+l2.extend(l3)
+print(l2)
+l4 = l3.copy()
+print(l4)
+l4.clear()
+print(l4)
