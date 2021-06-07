bb5505588bfbdf7f46e7300e94b5eb2a85827ffb
Chinmay Kulthe <chinmaykulthe897@gmail.com>
2021-06-07 15:52:29 +0530

    tut12, tut13 & tut14 added

---
+++ tut12.py
@@ -0,0 +1,2 @@
+# if 5>9: print("5 is greater than 9")
+print("5 is greater than 9") if 5>9 else print("9 greater than 5")
---
+++ tut13.py
@@ -0,0 +1,10 @@
+print(sum((56,5))) # Inbuilt function
+
+# User defined function
+def avg(n,n2):
+    """This function which will find avarage of given 2 numbers"""
+    print("This function will find avarage :")
+    return (n+n2)/2
+
+print(avg(57,61))
+print(avg.__doc__)
